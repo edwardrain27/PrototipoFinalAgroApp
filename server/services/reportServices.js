4e18ebc3dc067149c90b3c06f568de8b61616762
edwardrain27 <pablo522648@gmail.com>
2021-10-08 21:03:19 -0500

    consultas dinamicas

---
+++ server/services/reportServices.js
@@ -0,0 +1,25 @@
+
+const pool = require('../database');
+const {escape} = require('mysql');
+const { is } = require('bluebird');
+
+
+reporteTrabajadores = async({area,estado})=>{
+
+    let query = 'SELECT * FROM subterreno WHERE 1=1';
+
+    if(area) {
+        query+=` AND sub_area = ${area}`;
+    }
+    if(estado)
+    {   
+        query+=` AND sub_estado = ${estado}`;
+    }
+
+
+    return await pool.query(query);
+}
+
+module.exports = {
+    reporteTrabajadores
+}
